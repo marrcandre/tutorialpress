f4fd849260c2785a897b61d3c9c01694800e8254
Marco André Mendes <marcoandre@gmail.com>
2020-05-21 23:21:39 -0300

    Pequenas arrumações de organização do código

--- tutorialpress/core/models.py
+++ tutorialpress/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
 class Publicacao(models.Model):
     titulo = models.CharField(max_length=255)
     conteudo = models.TextField()
     is_publicada = models.BooleanField(default=False)
-
-
-class Categoria(models.Model):
-    nome = models.CharField(max_length=255)
--- tutorialpress/core/router.py
+++ tutorialpress/core/router.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 from tutorialpress.core.views import CategoriaViewSet, PublicacaoViewSet
 
 router = routers.SimpleRouter()
-router.register("publicacoes", PublicacaoViewSet)
 router.register("categorias", CategoriaViewSet)
+router.register("publicacoes", PublicacaoViewSet)
