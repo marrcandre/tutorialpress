6c86b527f6a462fd6e3991651213868c6f8a5b33
Marco André Mendes <marcoandre@gmail.com>
2020-05-21 23:35:16 -0300

    Relação entre categorias e publicações

--- tutorialpress/core/models.py
+++ tutorialpress/core/models.py
@@ -9,3 +9,11 @@ class Publicacao(models.Model):
     titulo = models.CharField(max_length=255)
     conteudo = models.TextField()
     is_publicada = models.BooleanField(default=False)
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.SET_NULL,
+        related_name="publicacoes",
+        related_query_name="publicacao",
+        null=True,
+        blank=True,
+    )
